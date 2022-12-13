a1a953d507e52a90d6e7fbdafce0394da4cb6d55
kaushiksaraf <kaushiksaraf91@gmail.com>
2022-12-13 23:41:41 +0530

    step-3 done

--- server.js
+++ server.js
@@ -9,10 +9,21 @@ app.get("/i",(req,res)=> {
     res.send("hi there, type above after the link /i/google to go to google homepage and /i/github to go to github page")
 })
 app.get ("/i/google",(req,res)=> {
-    res.send("Hers the link to Google Homepage ,www.google.com")
+    res.send("Heres the link to Google Homepage ,<a>www.google.com</a>")
 })
 app.get ("/i/github",(req,res)=> {
-    res.send("Hers the link to Github Homepage ,www.github.com")
+    res.send("Heres the link to Github Homepage ,www.github.com")
+})
+
+
+app.get("/r",(req,res)=> {
+    res.send("hi there, type above after the link /r/google to go to google homepage and /r/github to go to github page")
+})
+app.get ("/r/google",(req,res)=> {
+    res.redirect("https://google.com")
+})
+app.get ("/r/github",(req,res)=> {
+    res.redirect("https://github.com/")
 })
 const port = 5000
 app.listen(port,()=> {
